27777a63fe6e1af007f8d9e63a0e2d8f2e91af3e
akkondaveeti4710-jpg <akkondaveeti4710@markcubanai.org>
2026-03-11 22:01:37 -0500

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,8 @@
+def show_menu():
+    print("\n======TriageAI PRO======")
+    print("1. Register New Patient")
+    print("2. View Emergency Queue")
+    print("3. Hospital Analytics")
+    print("4. Exit")
+
+    return input("Select Option")
