d68a4269d038e1734cc68670d935f708309b0415
akkondaveeti4710-jpg <akkondaveeti4710@markcubanai.org>
2026-03-12 14:51:51 -0500

    Create triage_engine.py

---
+++ triage_engine.py
@@ -0,0 +1,43 @@
+from triage.vital_analyzer import analyze_vitals
+from risk_models.cardiac_model import cardiac_risk
+from risk_models.stroke_model import stroke_risk
+from risk_models.infection_model import infection_risk
+
+def run_triage(patient):
+
+    vitals_score = analyze_vitals(patient)
+
+    cardiac_score = cardiac_risk(patient)
+
+    stroke_score = stroke_risk(patient)
+
+    infection_score = infection_risk(patient)
+
+    total = vitals_score + cardiac_score + stroke_score + infection_score
+
+    if total >=12:
+        level = 1
+    elif total >=9:
+        level = 2
+    elif total >= 6:
+        level = 3
+    elif total >= 3:
+        level = 4
+    else:
+        level = 5
+
+    report = f""""
+Patient : {patient['name']}
+Age : {patient["age"]}
+
+Vitals Score : {vitals_score}
+Cardiac Risk : {cardiac_score}
+Stroke Risk : {stroke_score}
+Infection Risk : {infection_score}
+
+Total Risk Score: {total}
+
+Assigned TRIAGE LEVEL {level}
+"""""
+
+    return level, report
